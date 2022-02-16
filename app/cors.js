2aec3fe50fb9b06d2172c8e820ded89385d66696
duongpham17 <duongpham17@icloud.com>
2022-02-16 18:48:55 +0000

    first commit

--- app/cors.js
+++ app/cors.js
@@ -2,6 +2,26 @@ const cors = require('cors');
 
 module.exports = (app) => {
 
+
+    const whitelist = ['http://localhost:3000', "https://meusic-web-app.herokuapp.com"];
+
+    app.use(cors({
+        origin: whitelist,
+        credentials: true,
+        methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+        allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept'],
+        exposedHeaders: ["set-cookie"]
+    }));
+
+    app.use(cors({
+        origin: "*",
+        methods: ['GET']
+    }));
+}
+
+
+/**
+
     const whitelist = ['http://localhost:3000', "https://meusic-web-app.herokuapp.com"];
 
     app.use(cors({
@@ -12,4 +32,4 @@ module.exports = (app) => {
         exposedHeaders: ["set-cookie"]
     }));
 
-}
+ */
