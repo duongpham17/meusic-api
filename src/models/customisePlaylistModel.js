ee641b88d1f3c685c8300a4a105c805c60007dbf
duongpham17 <duongpham17@icloud.com>
2022-03-16 22:08:32 +0000

    model

--- src/models/customisePlaylistModel.js
+++ src/models/customisePlaylistModel.js
@@ -8,9 +8,38 @@ const customisePlaylistSchema = new mongoose.Schema({
         type: mongoose.Schema.ObjectId,
         ref: 'User',
     },
-    song: {
-        type: Array
-    },
+    song: [
+        {
+            title:{
+                type: String,
+            },
+            artist:{
+                type: String,
+            },
+            song: {
+                type: String,
+            },
+            url: {
+                type: String,
+                trim: true,
+            },
+            cid: {
+                type: String,
+                trim: true
+            },
+            duration: {
+                type: Number
+            },
+            image: {
+                type: String,
+                trim: true,
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now()
+            },
+        }
+    ],
     createdAt: {
         type: Date,
         default: Date.now()
